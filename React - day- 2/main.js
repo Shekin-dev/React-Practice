ce2d898216cfe294ce161f9ef9853565ac0437b9
Shekin Sampath R <shekinsampath.r@ind.xebiadigital.com>
2024-01-19 10:02:11 +0530

    Updated

--- React - day- 2/main.js
+++ React - day- 2/main.js
@@ -1,4 +1,15 @@
-const heading = React.createElement("h1", {}, "Hello React");
+/*const heading = React.createElement("h1", {}, "Hello React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(heading);*/
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", { id: "heading" }, "I am h1"),
+      React.createElement("h2", { id: "heading2" }, "I am h2"),
+    ])
+  );
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
 
