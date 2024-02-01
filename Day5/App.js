1a941519346191e35ee745108f54921f8b02d170
Shekin-dev <samkinbrothers2@gmail.com>
2024-02-01 12:45:52 +0530

    updated

--- Day5/App.js
+++ Day5/App.js
@@ -1,31 +1,82 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
-
+import resList from "./utilities/Datas";
 const Header = () => {
-  return(
-    <div className="Header">
-      <img src="" 
-      alt="Logo"
-      />
-      <ul className="Nav-terms">
-        <li> about us </li>
-        <li> Contact </li>
-          </ul>
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://cdn.dribbble.com/users/630677/screenshots/3833541/media/201454f743f48c415a38c49419275692.jpg?resize=400x0"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-const Restaurant =() =>{
-  return(
-    
-  )
-}
-const body =() =>{
+const RestaurantCard = (props) => {
+  const { resData } = props;
+  const { name, cuisines, cloudinaryImageId, costForTwo, sla, avgRating } =
+    resData?.info;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+        alt="res-logo"
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines?.join(",")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.deliveryTime} mins</h4>
+      {/* <h4>{resData?.info?.avgRating}</h4>
+      <h4>{resData?.info?.costForTwo}</h4>
+      <h4>{resData?.info?.sla?.deliveryTime} mins</h4>{" "} */}
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* restaurant-card */}
+        {resList.map((resObj) => (
+          <RestaurantCard resData={resObj} />
+        ))}
+        {/* For each restaurant, it is a function, it is returning a piece of jsx */}
+        {/* <RestaurantCard resData={resList[0]} />
+        <RestaurantCard resData={resList[1]} />
+        <RestaurantCard resData={resList[2]} />
+        <RestaurantCard resData={resList[3]} /> */}
+        {/* <RestaurantCard resData={resObj} /> */}
+        {/* <RestaurantCard resName="KFC" cuisine="Chicken, Fast Food" /> */}
+      </div>
+    </div>
+  );
+};
 
-}
+const AppLayout = () => {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header /> {/*Component Composition */}
+      {/* Body */}
+      <Body />
+      {/* Footer */}
+    </div>
+  );
+};
 
-const AppLayout = () =>{
-  return <div className="container">
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  </div>
-}
+root.render(<AppLayout />);
